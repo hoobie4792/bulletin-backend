0a8ff014ddb3882cee26f4a26bf01073cafae3bf
github username <github email address>
2020-06-29 21:34:53 -0400

    Added comment functionality

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -4,12 +4,26 @@ class Api::V1::CommentsController < ApplicationController
 
   def create
     if current_user
+      post = Post.find_by(id: post_params[:id]) || Post.find_by(
+        content: post_params[:content],
+        news_author: post_params[:news_author],
+        news_image: post_params[:news_image],
+        news_title: post_params[:news_title]
+      )
+      if !post
+        post = Post.new(post_params)
+        if !post.save
+          render :json => { message: post.errors.full_messages.join(', ')}, :status => :bad_request
+          return
+        end
+      end
       comment = Comment.new(comment_params)
+      comment.post = post
       comment.user = current_user
       if comment.save
-        render :json => comment.as_json(:only => [:id, :content, :created_at]), :status => :ok
+        render :json => post.serialized, :status => :ok
       else
-        render :json => { message: 'Could not create comment' }, :status => :bad_request
+        render :json => { message: comment.errors.full_messages.join(', ') }, :status => :bad_request
       end
     else
       render :json => { message: 'Must be logged in to create comment' }, :status => :unauthorized
@@ -45,7 +59,11 @@ class Api::V1::CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id)
+    params.require(:comment).permit(:content)
+  end
+
+  def post_params
+    params.require(:post).permit(:id, :content, :created_at, :news_author, :news_image, :news_source, :news_title, :news_url, :is_news_story)
   end
 
 end
