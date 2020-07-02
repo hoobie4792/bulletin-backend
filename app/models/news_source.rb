f763b91cc41cb0d614ca06c3e286d1d63cdd369c
Brandon Brown <hoobie4792@yahoo.com>
2020-07-02 09:21:16 -0400

    Only require name for news source and interest validations

--- app/models/news_source.rb
+++ app/models/news_source.rb
@@ -2,6 +2,6 @@ class NewsSource < ApplicationRecord
   has_many :user_news_sources
   has_many :users, :through => :user_news_sources
 
-  validates :name, :image, :url, :presence => true
+  validates :name, :presence => true
   validates :name, :uniqueness => true
 end
