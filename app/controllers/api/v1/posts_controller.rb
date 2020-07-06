81afafa9ee2b88cc3d45c772409994ab7773559b
github username <github email address>
2020-07-06 14:51:24 -0400

    Added share functionality

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -46,6 +46,36 @@ class Api::V1::PostsController < ApplicationController
     end
   end
 
+  def create_shared_post
+    if current_user
+      if shared_post_params[:shared_post][:id]
+        shared_post = Post.find_by(id: shared_post_params[:shared_post][:id])
+      else
+        shared_post = Post.create(shared_post_params[:shared_post].except(:id))
+      end
+      post = Post.new(content: shared_post_params[:content])
+      post.user = current_user
+      if post.save
+        post.current_user = current_user
+        post.create_notifications
+        Share.create(parent_post: post, shared_post: shared_post)
+        shared_post_params[:tags].each do |t| 
+          tag = Tag.find_by(name: t.downcase)
+          if !tag
+            tag = Tag.create(name: t.downcase)
+          end
+          PostTag.create(post: post, tag: tag)
+        end
+        
+        render :json => post.serialized, :status => :ok
+      else
+        render :json => { message: 'Could not create post' }, :status => :bad_request
+      end
+    else
+      render :json => { message: 'Must be logged in to share post' }, :status => :unauthorized
+    end
+  end
+
   def update
     if current_user
       post = Post.find_by(id: params[:id])
@@ -76,6 +106,12 @@ class Api::V1::PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:content, :tags => [])
+  end
+
+  def shared_post_params
+    params.require(:shared_post).permit(:content, :tags => [], :shared_post => [
+      :id, :content, :created_at, :news_author, :news_image, :news_source, :news_title, :news_url, :is_news_story
+    ])
   end
 
 end
